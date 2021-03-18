60974ccf0181eb9fbfd21406279b63993049429b
xx708@nyu.edu <xx708@nyu.edu>
2021-03-19 07:04:31 +0800

    Update: Search-related pages probably done

--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -1,26 +1,40 @@
 import './Search.css'
 import React, { useState, useEffect } from 'react'
-import searchResult from './searchResult'
-import axios from 'axios'
-
+// import searchResult from './SearchResult'
+import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom'
+// import axios from 'axios'
+import SearchRecommended from './SearchRecommended'
+import Trending from './Trending'
 
 const Search = (props) => {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
 
     return (
         <div className = "Search">
             <h2>Search Page here</h2>
             <search className = "main-content">
-
-
+                <SearchBar 
+                    searchQuery={searchQuery}
+                    setSearchQuery={setSearchQuery}
+                />
+                <Link to={'/searchResult'}> 
+                    <button>Showcasing SearchResult</button>
+                </Link>
+                    
             </search>
 
             <recommend>
                 <p>Recommended For You</p>
+                <SearchRecommended />
 
             </recommend>
 
             <trending>
-
+                <p>🔥Trending🔥</p>
+                <Trending />
             </trending>
         </div>
     );
