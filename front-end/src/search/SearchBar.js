5ada38e5ce8615859a04d832ffeca0c03a7c1592
xx708@nyu.edu <xx708@nyu.edu>
2021-03-19 06:59:09 +0800

    Update: Search Bar done

---
+++ front-end/src/search/SearchBar.js
@@ -0,0 +1,28 @@
+import { useHistory } from 'react-router-dom';
+import React from 'react'
+// import SearchResult from './SearchResult'
+import './SearchBar.css'
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
+    const history = useHistory();
+
+    const onSubmit = e => {
+        history.push(`?s=${searchQuery}`)
+        e.preventDefault()
+    };
+
+    return (
+        <form action="/" method="get">
+            <input
+                type = "text"
+                id = "header-search"
+                placeholder = "What do you want? "
+                name = "s" 
+            />
+
+            <button type = "submit" onSubmit={onSubmit}> Search </button>
+        </form>
+    );
+}    
+
+export default SearchBar;
