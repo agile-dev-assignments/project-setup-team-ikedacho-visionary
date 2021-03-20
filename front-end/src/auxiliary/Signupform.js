e65ca8056c9d6c16b74e319f8aa83597203a5d31
clairemaa <xm572@nyu.edu>
2021-03-20 18:58:39 +0800

    Added:Signup and login Page

---
+++ front-end/src/auxiliary/Signupform.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Loginform.css'
+
+const Signup = (props) => {
+
+    const SignUp_button = e => {
+        alert("Successfully Signed up")
+        }
+        
+    return (
+        <section class="section sign-in">  
+            <div className = "align-center">
+        <form action="">
+        <input type="text" name="name" placeholder="Name"/>
+        <input type="text" name="email" placeholder="Email"/>
+        <input type="password" name="password" placeholder="Password"/>
+        <input type="password" name="confirm" placeholder="Confirm Password"/>
+        <button onClick = {SignUp_button}>Create Account</button>
+        </form> 
+        </div>
+            </section>
+
+    );
+}
+
+export default Signup;
+
---
+++ front-end/src/login_signup/Signuppage.js
@@ -0,0 +1,24 @@
+import Signup from '../auxiliary/Signupform'
+import './Loginpage.css'
+import { NavLink } from 'react-router-dom'
+
+function Signuppage() {
+
+    return(
+    <div className="Loginpage">
+        <header class="section header">
+    
+        <div class="header__text">
+        <h1>O-Zone</h1>
+      <p>Sign in or create a new account.</p>
+    </div>
+  </header>
+        <div class = "Signupform">
+        <Signup/>
+        </div>
+        <NavLink id="signin" to = "/Login">Already have an account?</NavLink>
+        </div>
+    )
+}
+
+export default Signuppage;
