795af7d6406b1131ded26820a428f031e6cddf4c
lh2510 <lh2510@nyu.edu>
2021-03-21 03:48:56 -0400

    Add:add post content to my profile page. #64

--- front-end/src/me/MyProfile.js
+++ front-end/src/me/MyProfile.js
@@ -11,8 +11,8 @@ import { TextParagraph } from 'react-bootstrap-icons';
 import { HeartFill } from 'react-bootstrap-icons';
 import { Youtube } from 'react-bootstrap-icons';
 import { Linkedin } from 'react-bootstrap-icons';
-
-
+import axios from 'axios'
+import PostContent from '../auxiliary/PostContent'
 
 const MyProfile = (props) => {
     // start a state variable with a blank array
@@ -27,7 +27,22 @@ const MyProfile = (props) => {
         "following_number": "200",
         "linked_social_media": ["Facebook","Twitter","Instagram","TikTok"]
       })
-   
+      const [data, setData] = useState([])
+
+        // the following side-effect will be called once upon initial render
+        useEffect(() => {
+
+        axios('https://my.api.mockaroo.com/sr.json?key=2d6d6d60')
+            .then((response) => {
+                // extract the data from the server response
+                setData(response.data)
+            })
+            .catch((err) => {
+                const backupData = [{"source":"Konklux","userimg":"https://robohash.org/laboriosamaliquamconsequuntur.jpg?size=50x50\u0026set=set1","UserName":"mgalliard0","content":"Aaaahhhhhh! I do not know what to say. ","Senttime":"10/18/2020","contentimg":"http://dummyimage.com/112x136.jpg/cc0000/ffffff"},{"source":"Domainer","userimg":"https://robohash.org/utculpaesse.png?size=50x50\u0026set=set1","UserName":"hrevie1","content":"Aaaahhhhhhhhhhh! I do not know what to say. ","Senttime":"4/15/2020","contentimg":"http://dummyimage.com/228x124.bmp/cc0000/ffffff"},{"source":"Ventosanzap","userimg":"https://robohash.org/etquosa.bmp?size=50x50\u0026set=set1","UserName":"avedenisov2","content":"Aaaahhhhh! I do not know what to say. ","Senttime":"2/5/2021","contentimg":"http://dummyimage.com/107x217.jpg/5fa2dd/ffffff"},{"source":"Lotlux","userimg":"https://robohash.org/inciduntatest.png?size=50x50\u0026set=set1","UserName":"fhenniger3","content":"Aaaahhhhhhhhhhhhh! I do not know what to say. ","Senttime":"3/1/2021","contentimg":"http://dummyimage.com/219x227.jpg/5fa2dd/ffffff"},{"source":"Span","userimg":"https://robohash.org/quidemquicupiditate.bmp?size=50x50\u0026set=set1","UserName":"gmacqueen4","content":"Aaaahhhhhhhhhhhhhh! I do not know what to say. ","Senttime":"5/21/2020","contentimg":"http://dummyimage.com/185x108.bmp/dddddd/000000"},{"source":"Namfix","userimg":"https://robohash.org/dolorcumqueeaque.png?size=50x50\u0026set=set1","UserName":"gmorot5","content":"Aaaahhhhhhhhhhhh! I do not know what to say. ","Senttime":"9/27/2020","contentimg":"http://dummyimage.com/223x118.jpg/5fa2dd/ffffff"},{"source":"Overhold","userimg":"https://robohash.org/oditdolorenesciunt.png?size=50x50\u0026set=set1","UserName":"mmcileen6","content":"Aaaahhhhhhhhhhhh! I do not know what to say. ","Senttime":"6/17/2020","contentimg":"http://dummyimage.com/181x111.bmp/5fa2dd/ffffff"},{"source":"Pannier","userimg":"https://robohash.org/etnobisest.jpg?size=50x50\u0026set=set1","UserName":"gthrustle7","content":"Aaaahhhhhhhhhhhhhhh! I do not know what to say. ","Senttime":"5/2/2020","contentimg":"http://dummyimage.com/161x248.png/ff4444/ffffff"},{"source":"Overhold","userimg":"https://robohash.org/oditautet.jpg?size=50x50\u0026set=set1","UserName":"rlafond8","content":"Aaaahhhhhh! I do not know what to say. ","Senttime":"3/21/2020","contentimg":"http://dummyimage.com/123x113.png/5fa2dd/ffffff"},{"source":"Bamity","userimg":"https://robohash.org/autdeleniticonsequuntur.bmp?size=50x50\u0026set=set1","UserName":"ckarleman9","content":"Aaaahhhhhh! I do not know what to say. ","Senttime":"11/14/2020","contentimg":"http://dummyimage.com/234x151.jpg/cc0000/ffffff"}
+                ]
+                setData(backupData)
+            })
+        }, []) // only run it once!
     return (
         <div className = "MyProfile">
             <section id='header'>
@@ -80,7 +95,18 @@ const MyProfile = (props) => {
 
 
             </section>
-
+        <section>
+        <section className = "main-content4">
+                {data.map((item) => (
+                        <PostContent source = {item.source} 
+                                userimg = {user_info.user_photo}
+                                UserName = {user_info.user_name}
+                                content = {item.content}
+                                Senttime = {item.Senttime} 
+                                contentimg = {item.contentimg} />
+                    ))} 
+            </section>
+        </section>
          
 
         </div>
