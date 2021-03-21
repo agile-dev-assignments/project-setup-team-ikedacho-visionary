4af9b09542f2174bfcf5028f3689e7cd5bb5726a
lh2510 <lh2510@nyu.edu>
2021-03-21 15:39:32 -0400

    Fix:link img and username in my_comment_history page to my_profile page #64

--- front-end/src/me/My_Comment_History.js
+++ front-end/src/me/My_Comment_History.js
@@ -7,23 +7,27 @@ const My_Comment_History = (props) => {
 
   return (
     <article className="My_Comment_History">
-        <Link to={`/profile`}>
+       
             <div>
+              <Link to={`/my_profile`}>
                 <img class='inline-block' id="avatar" src="https://robohash.org/autplaceatatque.bmp?size=50x50" />
                 <h1 class='inline-block' id='username1' >Username</h1>
+                </Link>
                 <span id="commented_date">{props.details.commented_date}</span>
             </div>
         
         <p id='comment'>Commented @{props.details.post_created_by}: {props.details.commented_content}</p>
        
-        </Link>
+        
 
         <Link to={`/post_content/${props.details.post_text}`}>
             
                 
                 <p id='post' >
 
+            
                 <img id="post_image" src={props.details.post_image} />
+               
                 <br></br>
                 <span id='username'>{props.details.post_created_by}</span>
                 <br></br>
