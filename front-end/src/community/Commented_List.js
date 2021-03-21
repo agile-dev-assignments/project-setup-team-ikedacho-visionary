5efaf73336943faf6da3cb78ee211efee10c9eb8
lh2510 <lh2510@nyu.edu>
2021-03-21 02:23:39 -0400

    Fix:fix Commented_List.css

--- front-end/src/community/Commented_List.js
+++ front-end/src/community/Commented_List.js
@@ -14,7 +14,7 @@ const Commented_List = (props) => {
     // fetch some mock data about animals for sale
     console.log('fetching commented_history')
 
-    axios('https://my.api.mockaroo.com/comment_history.json?key=a2ecc78')
+    axios('https://my.api.mockaroo.com/comment_history.json?key=a2ecc780')
       .then((response) => {
         // extract the data from the server response
         setData(response.data)
@@ -26,27 +26,24 @@ const Commented_List = (props) => {
 
         // make some backup fake data of commented_history
         const backupData = [
-  
-
-          {"id":1,"post_text":"Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus.",
-          "post_image":"http://dummyimage.com/50x50.bmp/dddddd/000000",
-          "post_date":"7/17/2020",
-          "commented_by_username":"droobottom0",
-          "commented_by_profile_image":"https://robohash.org/dolorevoluptatemrecusandae.bmp?size=50x50\u0026set=set1",
-          "commented_date":"1/2/2021",
-          "commented_content":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
-          },
-          
-          {"id":2,"post_text":"Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue.",
-          "post_image":"http://dummyimage.com/50x50.png/ff4444/ffffff",
-          "post_date":"1/5/2020",
-          "commented_by_username":"lfruen1",
-          "commented_by_profile_image":"https://robohash.org/remplaceatcommodi.png?size=50x50\u0026set=set1",
-          "commented_date":"7/20/2020",
-          "commented_content":"Integer a nibh. In quis justo."
-          },
-          {"id":3,"post_text":"Fusce consequat. Nulla nisl. Nunc nisl.","post_image":"https://robohash.org/rerumdoloreaque.jpg?size=50x50\u0026set=set1","post_date":"1/19/2021","commented_by_username":"lloweth2","commented_by_profile_image":"http://dummyimage.com/50x50.bmp/ff4444/ffffff","commented_date":"4/13/2020","commented_content":"Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede."},{"id":4,"post_text":"Maecenas ut massa quis augue luctus tincidunt.","post_image":"https://robohash.org/rerumaccusantiumveritatis.bmp?size=50x50\u0026set=set1","post_date":"2/6/2020","commented_by_username":"eodulchonta3","commented_by_profile_image":"http://dummyimage.com/50x50.bmp/cc0000/ffffff","commented_date":"5/12/2020","commented_content":"Vestibulum sed magna at nunc commodo placerat."},{"id":5,"post_text":"Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.","post_image":"https://robohash.org/velsimiliqueaspernatur.png?size=50x50\u0026set=set1","post_date":"7/15/2020","commented_by_username":"istill4","commented_by_profile_image":"http://dummyimage.com/50x50.jpg/5fa2dd/ffffff","commented_date":"10/21/2020","commented_content":"Nunc rhoncus dui vel sem. Sed sagittis."},{"id":6,"post_text":"In congue. Etiam justo.","post_image":"https://robohash.org/solutaavelit.png?size=50x50\u0026set=set1","post_date":"11/10/2020","commented_by_username":"tmcreidy5","commented_by_profile_image":"http://dummyimage.com/50x50.bmp/cc0000/ffffff","commented_date":"4/4/2020","commented_content":"Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui."},{"id":7,"post_text":"Pellentesque at nulla.","post_image":"https://robohash.org/estdoloremquevoluptas.jpg?size=50x50\u0026set=set1","post_date":"8/26/2020","commented_by_username":"srisley6","commented_by_profile_image":"http://dummyimage.com/50x50.jpg/5fa2dd/ffffff","commented_date":"11/16/2020","commented_content":"In hac habitasse platea dictumst."},{"id":8,"post_text":"Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio.","post_image":"https://robohash.org/etcorruptiplaceat.bmp?size=50x50\u0026set=set1","post_date":"4/13/2020","commented_by_username":"kdoblin7","commented_by_profile_image":"http://dummyimage.com/50x50.png/cc0000/ffffff","commented_date":"3/14/2020","commented_content":"Nunc rhoncus dui vel sem."},{"id":9,"post_text":"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum.","post_image":"https://robohash.org/dictacorporisaliquid.bmp?size=50x50\u0026set=set1","post_date":"10/26/2020","commented_by_username":"cupfold8","commented_by_profile_image":"http://dummyimage.com/50x50.png/cc0000/ffffff","commented_date":"10/19/2020","commented_content":"Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor."},{"id":10,"post_text":"Nunc purus.","post_image":"https://robohash.org/voluptasnemoipsam.bmp?size=50x50\u0026set=set1","post_date":"5/1/2020","commented_by_username":"scalliss9","commented_by_profile_image":"http://dummyimage.com/50x50.png/cc0000/ffffff","commented_date":"9/21/2020","commented_content":"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros."}]
-
+        {"id":1,
+        "post_text":"Vestibulum ac est lacinia nisi venenatis tristique.",
+        "post_image":"http://dummyimage.com/80x80.bmp/cc0000/ffffff",
+        "post_date":"1/16/2020",
+        "commented_by_username":"mborell0",
+        "commented_by_profile_image":"https://robohash.org/occaecatiutfacilis.jpg?size=50x50\u0026set=set1",
+        "commented_date":"4/14/2020"
+        ,"commented_content":"Proin eu mi."},
+        {"id":2,
+        "post_text":"Integer ac neque.",
+        "post_image":"http://dummyimage.com/80x80.bmp/5fa2dd/ffffff",
+        "post_date":"7/29/2020",
+        "commented_by_username":"tkrates1",
+        "commented_by_profile_image":"https://robohash.org/doloremagniconsequatur.png?size=50x50\u0026set=set1",
+        "commented_date":"4/21/2020",
+        "commented_content":"Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl."},
+        {"id":3,"post_text":"Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque.","post_image":"http://dummyimage.com/80x80.jpg/ff4444/ffffff","post_date":"1/9/2021","commented_by_username":"ecumbes2","commented_by_profile_image":"https://robohash.org/accusantiumsedquasi.bmp?size=50x50\u0026set=set1","commented_date":"12/5/2020","commented_content":"Nullam sit amet turpis elementum ligula vehicula consequat."},{"id":4,"post_text":"Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim.","post_image":"http://dummyimage.com/80x80.png/dddddd/000000","post_date":"11/19/2020","commented_by_username":"kpirt3","commented_by_profile_image":"https://robohash.org/omnisidconsequatur.bmp?size=50x50\u0026set=set1","commented_date":"9/25/2020","commented_content":"Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est."},{"id":5,"post_text":"Curabitur convallis.","post_image":"http://dummyimage.com/80x80.jpg/dddddd/000000","post_date":"2/13/2021","commented_by_username":"bbarehead4","commented_by_profile_image":"https://robohash.org/praesentiumassumendadoloremque.bmp?size=50x50\u0026set=set1","commented_date":"5/28/2020","commented_content":"Morbi a ipsum."},{"id":6,"post_text":"Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy.","post_image":"http://dummyimage.com/80x80.bmp/cc0000/ffffff","post_date":"9/30/2020","commented_by_username":"mdowtry5","commented_by_profile_image":"https://robohash.org/quinatusipsum.png?size=50x50\u0026set=set1","commented_date":"12/26/2020","commented_content":"Proin eu mi."},{"id":7,"post_text":"Sed ante. Vivamus tortor.","post_image":"http://dummyimage.com/80x80.bmp/dddddd/000000","post_date":"4/21/2020","commented_by_username":"iwalcot6","commented_by_profile_image":"https://robohash.org/rerumquisquammaxime.jpg?size=50x50\u0026set=set1","commented_date":"3/19/2020","commented_content":"Sed ante."},{"id":8,"post_text":"Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla.","post_image":"http://dummyimage.com/80x80.bmp/dddddd/000000","post_date":"10/27/2020","commented_by_username":"kkeble7","commented_by_profile_image":"https://robohash.org/voluptatemsitvoluptas.jpg?size=50x50\u0026set=set1","commented_date":"2/3/2021","commented_content":"Curabitur gravida nisi at nibh."},{"id":9,"post_text":"Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.","post_image":"http://dummyimage.com/80x80.jpg/dddddd/000000","post_date":"7/15/2020","commented_by_username":"cingre8","commented_by_profile_image":"https://robohash.org/etdoloreaut.bmp?size=50x50\u0026set=set1","commented_date":"10/18/2020","commented_content":"Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa."},{"id":10,"post_text":"Proin eu mi. Nulla ac enim.","post_image":"http://dummyimage.com/80x80.bmp/dddddd/000000","post_date":"9/3/2020","commented_by_username":"cboodle9","commented_by_profile_image":"https://robohash.org/maximenemodolor.png?size=50x50\u0026set=set1","commented_date":"4/21/2020","commented_content":"Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet."}
+        ]
         setData(backupData)
       })
   }, []) // only run it once!
