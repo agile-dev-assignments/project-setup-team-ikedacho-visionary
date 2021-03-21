179f1e1536578aa4eccb2590e18c2b02fc5fd429
Skylar Gao <jg5380@nyu.edu>
2021-03-21 13:14:31 +0800

    implemented browse history page: user story #28

---
+++ front-end/src/auxiliary/Browse.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+import './Browse.css'
+
+const Browse = (props) => {
+    return (
+        <div className="Browse">
+            <div className="block">
+                <img className="userimg" src={props.userimg}/>
+                <div className="Text">
+                    <strong className="username">{props.UserName}</strong>
+                    <p className="content">{props.content}</p>
+                </div>
+            </div>
+            <div className="imgs">
+                {props.contentimgs.map((item) => (
+                    <img className="img" src={item} alt=""/>
+                ))}
+            </div>
+            {/*<img className="" src={props.contentimg}/>*/}
+        </div>
+    )
+}
+
+
+export default Browse
