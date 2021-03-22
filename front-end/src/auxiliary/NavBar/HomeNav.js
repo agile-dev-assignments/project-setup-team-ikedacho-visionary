eaa52d01520288f596e6bb8563fb200d5d87f09d
Skylar Gao <jg5380@nyu.edu>
2021-03-22 10:14:07 +0800

    Added 'filter' and 'new post' buttons to home nav

--- front-end/src/auxiliary/NavBar/HomeNav.js
+++ front-end/src/auxiliary/NavBar/HomeNav.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 import './HomeNav.css'
+import {FileEarmarkPlus, SortDown} from "react-bootstrap-icons";
 
 // Login page is placed here for building/inspecting purpose
-// It will not appear in the Navigation bar in finalized version 
+// It will not appear in the Navigation bar in finalized version
 const HomeNav = (props) => {
     return (
-        <nav className = "homenav">
+        <nav className="homenav">
+            <NavLink id='homenav_button' to="/">
+                <SortDown color='black' size={17}/>
+            </NavLink>
             <NavLink id='homenav_button' to="/">What's New</NavLink>
             <NavLink id='homenav_button' to="/recent">Recently Visited</NavLink>
             <NavLink id='homenav_button' to="/recommend">Recommend</NavLink>
+            <NavLink id='homenav_button' to="/edit">
+                <FileEarmarkPlus id="create_post" color='black' size={17}/>
+            </NavLink>
         </nav>
     )
 }
