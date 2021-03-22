0fee72618bfb74435058b9691ac0e783ce844be7
lh2510 <lh2510@nyu.edu>
2021-03-21 20:38:00 -0400

    add current time when the comment sent in ToComment.js and css#78

--- front-end/src/auxiliary/ToComment.js
+++ front-end/src/auxiliary/ToComment.js
@@ -1,15 +1,44 @@
 import './ToComment.css'
-import React from 'react'
 
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const ToComment = (props) => {
+    const [show, setShow] = useState('true')
+
+    const handleClick=()=>{
+        console.log('clicked!')
+        setShow(!show);
+    }
+
+    const getTime=()=>{
+        let today = new Date()
+
+        return today;
+    }
 
     return (
-        <>
-      
-            <input placeholder="   write your comment here" type = "text" id = "repost_edit_text"/>
+        < >
         
-            <button id= "sendPost_edit">Send</button>
+            <input id = "repost_edit_text" style={{ display: show ? "block" : "none" }} placeholder="   write your comment here" type = "text" / >
+        
+            <button id= "sendPost_edit" onClick={handleClick} style={{ display: show ? "block" : "none" } }>Send</button>
+        
+        
+        <div id='mycomment' style={{ display: !show ? "block" : "none" }}>
+            <div id='userphoto' style={{ display: !show ? "block" : "none" }}>
+           
+                <Link  to = {'/my_profile'}>
+                    <span id='username' >Joe:</span>
+                    
+                </Link>
+                my comments
+            </div>
+           
+        </div>
+        <div id='date_send' style={{ display: !show ? "block" : "none" }}>
+            { getTime().toLocaleString() }
+        </div>
         </>
     )
 }
