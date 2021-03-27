172ce65c683e877fbb9c8b687a6a9fe869022abd
xx708@nyu.edu <xx708@nyu.edu>
2021-03-27 14:27:26 +0800

    Update: complete friend suggestion search process

--- back-end/app.js
+++ back-end/app.js
@@ -40,15 +40,15 @@ app.get("/my_comment_history", (req, res,next) => {
         .catch((err) => next(err))
             // temporary workaround because mockaro daily usage limit is exhausted at the moment...
 })
+
 app.get("/followers", (req, res) => {
     const response = axios
         .get(`${process.env.API_FOLLOWER_LIST}?key=${process.env.API_FOLLOWER_LIST_KEY}`)
         .catch((err) => {/*console.log(err)*/})
-    console.log(response)
-    // temporary workaround because mockaro daily usage limit is exhausted at the moment...
-    const backupData = [{"img":"https://robohash.org/animirationequia.bmp?size=50x50\u0026set=set1","UserName":"express.js","bio":"p v g t d W U J W w ","action":"unfollow"},{"img":"https://robohash.org/sedquosequi.bmp?size=50x50\u0026set=set1","UserName":"abalser1","bio":"k e x N L B C M S u ","action":"unfollow"},{"img":"https://robohash.org/ullamvoluptasiure.png?size=50x50\u0026set=set1","UserName":"agretton2","bio":"B C I S O L G x U J ","action":"unfollow"},{"img":"https://robohash.org/quiperferendisdistinctio.jpg?size=50x50\u0026set=set1","UserName":"cchaffin3","bio":"j D N E t y F X N I ","action":"unfollow"},{"img":"https://robohash.org/doloremetquaerat.png?size=50x50\u0026set=set1","UserName":"sbrosenius4","bio":"P D n U y n j E b E ","action":"unfollow"},{"img":"https://robohash.org/doloribusfugitest.png?size=50x50\u0026set=set1","UserName":"koskehan5","bio":"s b z h i R M C b W ","action":"unfollow"},{"img":"https://robohash.org/totamvoluptasoccaecati.png?size=50x50\u0026set=set1","UserName":"afrackiewicz6","bio":"u C V V n q f h C t ","action":"unfollow"},{"img":"https://robohash.org/undeevenietquidem.jpg?size=50x50\u0026set=set1","UserName":"wghidetti7","bio":"r v x W x V Y k j E ","action":"unfollow"},{"img":"https://robohash.org/porroautut.png?size=50x50\u0026set=set1","UserName":"lswaby8","bio":"D h m L d E W G j r ","action":"unfollow"},{"img":"https://robohash.org/dignissimosillumplaceat.jpg?size=50x50\u0026set=set1","UserName":"lclemerson9","bio":"D S z Y s l g z g r ","action":"unfollow"}
-                ]
 
+    // temporary workaround because mockaro daily usage limit is exhausted at the moment...
+    const backupData = [{"img":"https://robohash.org/animirationequia.bmp?size=50x50\u0026set=set1","UserName":"express.js follower","bio":"p v g t d W U J W w ","action":"unfollow"},{"img":"https://robohash.org/sedquosequi.bmp?size=50x50\u0026set=set1","UserName":"abalser1","bio":"k e x N L B C M S u ","action":"unfollow"},{"img":"https://robohash.org/ullamvoluptasiure.png?size=50x50\u0026set=set1","UserName":"agretton2","bio":"B C I S O L G x U J ","action":"unfollow"},{"img":"https://robohash.org/quiperferendisdistinctio.jpg?size=50x50\u0026set=set1","UserName":"cchaffin3","bio":"j D N E t y F X N I ","action":"unfollow"},{"img":"https://robohash.org/doloremetquaerat.png?size=50x50\u0026set=set1","UserName":"sbrosenius4","bio":"P D n U y n j E b E ","action":"unfollow"},{"img":"https://robohash.org/doloribusfugitest.png?size=50x50\u0026set=set1","UserName":"koskehan5","bio":"s b z h i R M C b W ","action":"unfollow"},{"img":"https://robohash.org/totamvoluptasoccaecati.png?size=50x50\u0026set=set1","UserName":"afrackiewicz6","bio":"u C V V n q f h C t ","action":"unfollow"},{"img":"https://robohash.org/undeevenietquidem.jpg?size=50x50\u0026set=set1","UserName":"wghidetti7","bio":"r v x W x V Y k j E ","action":"unfollow"},{"img":"https://robohash.org/porroautut.png?size=50x50\u0026set=set1","UserName":"lswaby8","bio":"D h m L d E W G j r ","action":"unfollow"},{"img":"https://robohash.org/dignissimosillumplaceat.jpg?size=50x50\u0026set=set1","UserName":"lclemerson9","bio":"D S z Y s l g z g r ","action":"unfollow"}
+                ]
     res.json(backupData)
 })
 
@@ -56,31 +56,44 @@ app.get("/followings", (req, res) => {
     const response = axios
         .get(`${process.env.API_FOLLOWING_LIST}?key=${process.env.API_FOLLOWING_LIST_KEY}`)
         .catch((err) => {/*console.log(err)*/})
-    console.log(response)
-    // temporary workaround because mockaro daily usage limit is exhausted at the moment...
-    const backupData = [{"img":"https://robohash.org/animirationequia.bmp?size=50x50\u0026set=set1","UserName":"following","bio":"p v g t d W U J W w ","action":"unfollow"},{"img":"https://robohash.org/sedquosequi.bmp?size=50x50\u0026set=set1","UserName":"abalser1","bio":"k e x N L B C M S u ","action":"unfollow"},{"img":"https://robohash.org/ullamvoluptasiure.png?size=50x50\u0026set=set1","UserName":"agretton2","bio":"B C I S O L G x U J ","action":"unfollow"},{"img":"https://robohash.org/quiperferendisdistinctio.jpg?size=50x50\u0026set=set1","UserName":"cchaffin3","bio":"j D N E t y F X N I ","action":"unfollow"},{"img":"https://robohash.org/doloremetquaerat.png?size=50x50\u0026set=set1","UserName":"sbrosenius4","bio":"P D n U y n j E b E ","action":"unfollow"},{"img":"https://robohash.org/doloribusfugitest.png?size=50x50\u0026set=set1","UserName":"koskehan5","bio":"s b z h i R M C b W ","action":"unfollow"},{"img":"https://robohash.org/totamvoluptasoccaecati.png?size=50x50\u0026set=set1","UserName":"afrackiewicz6","bio":"u C V V n q f h C t ","action":"unfollow"},{"img":"https://robohash.org/undeevenietquidem.jpg?size=50x50\u0026set=set1","UserName":"wghidetti7","bio":"r v x W x V Y k j E ","action":"unfollow"},{"img":"https://robohash.org/porroautut.png?size=50x50\u0026set=set1","UserName":"lswaby8","bio":"D h m L d E W G j r ","action":"unfollow"},{"img":"https://robohash.org/dignissimosillumplaceat.jpg?size=50x50\u0026set=set1","UserName":"lclemerson9","bio":"D S z Y s l g z g r ","action":"unfollow"}
-                ]
 
+    // temporary workaround because mockaro daily usage limit is exhausted at the moment...
+    const backupData = [{"img":"https://robohash.org/animirationequia.bmp?size=50x50\u0026set=set1","UserName":"express.js following","bio":"p v g t d W U J W w ","action":"unfollow"},{"img":"https://robohash.org/sedquosequi.bmp?size=50x50\u0026set=set1","UserName":"abalser1","bio":"k e x N L B C M S u ","action":"unfollow"},{"img":"https://robohash.org/ullamvoluptasiure.png?size=50x50\u0026set=set1","UserName":"agretton2","bio":"B C I S O L G x U J ","action":"unfollow"},{"img":"https://robohash.org/quiperferendisdistinctio.jpg?size=50x50\u0026set=set1","UserName":"cchaffin3","bio":"j D N E t y F X N I ","action":"unfollow"},{"img":"https://robohash.org/doloremetquaerat.png?size=50x50\u0026set=set1","UserName":"sbrosenius4","bio":"P D n U y n j E b E ","action":"unfollow"},{"img":"https://robohash.org/doloribusfugitest.png?size=50x50\u0026set=set1","UserName":"koskehan5","bio":"s b z h i R M C b W ","action":"unfollow"},{"img":"https://robohash.org/totamvoluptasoccaecati.png?size=50x50\u0026set=set1","UserName":"afrackiewicz6","bio":"u C V V n q f h C t ","action":"unfollow"},{"img":"https://robohash.org/undeevenietquidem.jpg?size=50x50\u0026set=set1","UserName":"wghidetti7","bio":"r v x W x V Y k j E ","action":"unfollow"},{"img":"https://robohash.org/porroautut.png?size=50x50\u0026set=set1","UserName":"lswaby8","bio":"D h m L d E W G j r ","action":"unfollow"},{"img":"https://robohash.org/dignissimosillumplaceat.jpg?size=50x50\u0026set=set1","UserName":"lclemerson9","bio":"D S z Y s l g z g r ","action":"unfollow"}
+                ]
     res.json(backupData)
 })
 
+app.get("/friend_suggestion", (req, res) => {
+    const response = axios
+        .get(`${process.env.API_FRIEND_SUGGESTION}?key=${process.env.API_FRIEND_SUGGESTION_KEY}`)
+        .catch((err) => {/*console.log(err)*/})
+
+    // temporary workaround because mockaro daily usage limit is exhausted at the moment...
+    // NOTE THAT THIS IS JUST A TEMPORARY SOLUTION FOR THE MOCKAROO LIMIT
+    const backupData_suggestion = [{"img":"https://robohash.org/animirationequia.bmp?size=50x50\u0026set=set1","UserName":"express.js friend_suggestion","bio":"p v g t d W U J W w ","action":"follow"},{"img":"https://robohash.org/sedquosequi.bmp?size=50x50\u0026set=set1","UserName":"abalser1","bio":"k e x N L B C M S u ","action":"follow"},{"img":"https://robohash.org/ullamvoluptasiure.png?size=50x50\u0026set=set1","UserName":"agretton2","bio":"B C I S O L G x U J ","action":"follow"},{"img":"https://robohash.org/quiperferendisdistinctio.jpg?size=50x50\u0026set=set1","UserName":"cchaffin3","bio":"j D N E t y F X N I ","action":"follow"},{"img":"https://robohash.org/doloremetquaerat.png?size=50x50\u0026set=set1","UserName":"sbrosenius4","bio":"P D n U y n j E b E ","action":"follow"},{"img":"https://robohash.org/doloribusfugitest.png?size=50x50\u0026set=set1","UserName":"koskehan5","bio":"s b z h i R M C b W ","action":"follow"},{"img":"https://robohash.org/totamvoluptasoccaecati.png?size=50x50\u0026set=set1","UserName":"afrackiewicz6","bio":"u C V V n q f h C t ","action":"follow"},{"img":"https://robohash.org/undeevenietquidem.jpg?size=50x50\u0026set=set1","UserName":"wghidetti7","bio":"r v x W x V Y k j E ","action":"follow"},{"img":"https://robohash.org/porroautut.png?size=50x50\u0026set=set1","UserName":"lswaby8","bio":"D h m L d E W G j r ","action":"follow"},{"img":"https://robohash.org/dignissimosillumplaceat.jpg?size=50x50\u0026set=set1","UserName":"lclemerson9","bio":"D S z Y s l g z g r ","action":"follow"}
+                ]
+    const backupData_searched = [{"img":"https://robohash.org/animirationequia.bmp?size=50x50\u0026set=set1","UserName": `${req.query.searched_name}`,"bio":"p v g t d W U J W w ","action":"follow"}
+                ]
+
+    const ret = {
+        result: (req.query.type == "Searched Result") ? backupData_searched : backupData_suggestion,
+        type: req.query.type
+    }
+
+    console.log(ret.type)
+
+    res.json(ret)
+})
+
+app.get("/getprelogin", (req, res,next) => {
+    axios
+        .get(`https://my.api.mockaroo.com/postcontent.json?key=a4705650`)
+        .then(response=> res.json(response.data))
+        .catch((err) => next(err))
+            // temporary workaround because mockaro daily usage limit is exhausted at the moment...
+})
 
 
-app.get("/getprelogin",(req,res) => {
-    request(
-        "https://my.api.mockaroo.com/sr.json?key=2d6d6d60",
-        function(error,response,body){
-            if(!error){
-                
-                
-                 var parseBody = JSON.parse(body);
-                 res.send(parseBody);
-
-            }
-        }
-
-    );
-});
 
 
 function isEmpty(obj) {
--- front-end/src/me/Friend_Suggestion.js
+++ front-end/src/me/Friend_Suggestion.js
@@ -2,92 +2,43 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import NameTag from "../auxiliary/NameTag";
+import { Search } from 'react-bootstrap-icons';
 import "./Friend_Suggestion.css";
 
 const Friend_Suggestion = (props) => {
   const [data, setData] = useState([]);
+  const [title, setTitle] = useState(["People You Might Know"]);
+  const [search_name, setSearch_name] = useState([]);
 
   useEffect(() => {
-    axios("https://my.api.mockaroo.com/followings.json?key=2d6d6d60")
+    // previous developer used my mockaroo API...:
+    // "https://my.api.mockaroo.com/followings.json?key=2d6d6d60"
+    axios
+      .get('/friend_suggestion', {
+            params: {
+              type: title,
+              searched_name: search_name
+            }
+          })
       .then((response) => {
         // extract the data from the server response
-        setData(response.data);
+        setData(response.data.result);
+        setTitle(response.data.type)
       })
       .catch((err) => {
-        const backupData = [
-          {
-            img: "https://robohash.org/etillumet.bmp?size=50x50\u0026set=set1",
-            UserName: "rmorcombe0",
-            bio: "I c M J k Z p l o B ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/etadipiscitempore.bmp?size=50x50\u0026set=set1",
-            UserName: "ghort1",
-            bio: "Q z O A y p s i g W ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/suntdebitispossimus.png?size=50x50\u0026set=set1",
-            UserName: "kstuckey2",
-            bio: "c q t R E c I J a s ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/dolorematsaepe.jpg?size=50x50\u0026set=set1",
-            UserName: "jpigden3",
-            bio: "T m P I T T n H C i ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/inciduntillosed.png?size=50x50\u0026set=set1",
-            UserName: "kdennis4",
-            bio: "B N W i T X X b Z f ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/porronobispraesentium.png?size=50x50\u0026set=set1",
-            UserName: "ccribbott5",
-            bio: "G P n F f Z C D L I ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/voluptatedoloreaut.bmp?size=50x50\u0026set=set1",
-            UserName: "ccrutchley6",
-            bio: "x Y H H F j M u Q P ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/quisequisit.jpg?size=50x50\u0026set=set1",
-            UserName: "fvamplus7",
-            bio: "P Y F z p I K J V G ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/rationeeasaepe.jpg?size=50x50\u0026set=set1",
-            UserName: "cfarries8",
-            bio: "M r p k d F m v Y W ",
-            action: "Follow",
-          },
-          {
-            img:
-              "https://robohash.org/erroraliquammollitia.jpg?size=50x50\u0026set=set1",
-            UserName: "mgorham9",
-            bio: "Y R d D H x z H H b ",
-            action: "Follow",
-          },
-        ];
-        setData(backupData);
+        /*
+        const backupData = []
+        setData(backupData);*/
       });
-  }, []); // only run it once!
+  }, [search_name]); // only run it once!
+
+  const _setTitle_Search_name = (val) => {
+    setSearch_name(val)
+    setTitle("Searched Result")
+    if (val == '') {
+      setTitle("People You Might Know")
+    }
+  }
 
   return (
     <div className="Friend_Suggestion">
@@ -100,13 +51,13 @@ const Friend_Suggestion = (props) => {
       </header>
 
       <div className="input-wrap">
-        <input className="input" type="text" placeholder="Search by user here"/>
-        <img  src="" alt=""/>
-        <span className="icon">&times;</span>
+        <input className="input" type="search" placeholder="Search by user here"
+               value = {search_name} onInput = {e => _setTitle_Search_name(e.target.value)}/>
+        <Search id='search_icon' color="grey" size={15} />
       </div>
 
       <section className="main-content">
-        <p className="desc">people you might know</p>
+        <p className="desc">{title}</p>
         <p>
           {data.map((item) => (
             <NameTag
