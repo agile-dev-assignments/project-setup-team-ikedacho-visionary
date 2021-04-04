a50ad109261f19bdae2f8977e91006176291a74e
lh2510 <lh2510@nyu.edu>
2021-04-04 01:58:48 -0400

    Update: ToComment css. #188

--- front-end/src/auxiliary/ToComment.js
+++ front-end/src/auxiliary/ToComment.js
@@ -9,22 +9,26 @@ const ToComment = (props) => {
     const [show, setShow] = useState('true')
     const [currentTime, setCurrentTime] = useState('')
 
-       //represnet if the send button is clicked. false: not clicked. true: clicked
+    //represnet if the send button is clicked. false: not clicked. true: clicked
     const [send, setSend]=  useState(false)
-    let [comment_text, setComment_text]=  useState("default comment")
+    let [comment_text, setComment_text]=  useState("")
 
 
 
     const goTOPreviousPath2=()=>{
-        setSend(!send)
+        
+            setSend(!send)
+        
+       
        // setComment_text('my comment_text')
         console.log("send:",send)
         console.log("comment_text:",comment_text)
-        comment_text =  document.getElementById("myTextarea").value;
+        comment_text =  document.getElementById("myTextarea_ToComment").value;
         console.log(comment_text)
         console.log('clicked!')
-
+        if( comment_text!==""){
         setShow(!show);
+        }
     
         setCurrentTime(new Date());
          
@@ -32,7 +36,7 @@ const ToComment = (props) => {
 
     return (
         < >
-        <textarea style={{ display: show ? "block" : "none" }}  id="myTextarea" placeholder=" What's your comment?" onInput = {e => setComment_text(e.target.value)}/>
+        <textarea style={{ display: show ? "block" : "none" }}  id="myTextarea_ToComment" placeholder=" What's your comment?" onInput = {e => setComment_text(e.target.value)}/>
 
         <button id= "sendPost_edit" onClick={goTOPreviousPath2} style={{ display: show ? "block" : "none" } }>Send</button>
         
