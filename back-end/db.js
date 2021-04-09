607e2e230fdece440d1169cc11ca2a076293860e
lh2510 <57370805+lh2510@users.noreply.github.com>
2021-04-08 23:41:15 -0400

    Update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -2,14 +2,8 @@ const mongoose = require('mongoose')
 require('dotenv').config({ silent: true })
 const MongoClient = require('mongodb').MongoClient
 const uri = `${process.env.DB_URL}`
-const client = new MongoClient(uri, {
+mongoose.connect(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-})
-client.connect((err) => {
-	if (err) {
-		console.log(err)
-	} else {
-		console.log('success connect to mongodb')
-	}
+	useFindAndModify: false,
 })
