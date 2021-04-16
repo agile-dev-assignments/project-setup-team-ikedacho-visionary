3f8f65d000dfda3f2cc52d1516ab230091b5df00
xx708@nyu.edu <xx708@nyu.edu>
2021-04-17 01:26:31 +0800

    Update: minor syntax tweak

--- front-end/src/me/Followings.js
+++ front-end/src/me/Followings.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import './Followings.css'
 import { Link, useLocation, useHistory} from 'react-router-dom'
 import NameTag from '../auxiliary/NameTag'
-import './Followings.css'
 
 const Followings = (props) => {
     const [data, setData] = useState([])
@@ -16,7 +15,6 @@ const Followings = (props) => {
     }
 
     useEffect(() => {
-        // 'https://my.api.mockaroo.com/followings.json?key=2d6d6d60'
         axios
             .get('/api_followings', {
                 params: {
@@ -28,9 +26,7 @@ const Followings = (props) => {
                 setData(response.data)
             })
             .catch((err) => {
-                /*const backupData = [{"img":"https://robohash.org/etillumet.bmp?size=50x50\u0026set=set1","UserName":"rmorcombe0","bio":"I c M J k Z p l o B ","action":"unfollow"},{"img":"https://robohash.org/etadipiscitempore.bmp?size=50x50\u0026set=set1","UserName":"ghort1","bio":"Q z O A y p s i g W ","action":"unfollow"},{"img":"https://robohash.org/suntdebitispossimus.png?size=50x50\u0026set=set1","UserName":"kstuckey2","bio":"c q t R E c I J a s ","action":"unfollow"},{"img":"https://robohash.org/dolorematsaepe.jpg?size=50x50\u0026set=set1","UserName":"jpigden3","bio":"T m P I T T n H C i ","action":"unfollow"},{"img":"https://robohash.org/inciduntillosed.png?size=50x50\u0026set=set1","UserName":"kdennis4","bio":"B N W i T X X b Z f ","action":"unfollow"},{"img":"https://robohash.org/porronobispraesentium.png?size=50x50\u0026set=set1","UserName":"ccribbott5","bio":"G P n F f Z C D L I ","action":"unfollow"},{"img":"https://robohash.org/voluptatedoloreaut.bmp?size=50x50\u0026set=set1","UserName":"ccrutchley6","bio":"x Y H H F j M u Q P ","action":"unfollow"},{"img":"https://robohash.org/quisequisit.jpg?size=50x50\u0026set=set1","UserName":"fvamplus7","bio":"P Y F z p I K J V G ","action":"unfollow"},{"img":"https://robohash.org/rationeeasaepe.jpg?size=50x50\u0026set=set1","UserName":"cfarries8","bio":"M r p k d F m v Y W ","action":"unfollow"},{"img":"https://robohash.org/erroraliquammollitia.jpg?size=50x50\u0026set=set1","UserName":"mgorham9","bio":"Y R d D H x z H H b ","action":"unfollow"}
-                ]
-                setData(backupData)*/
+                console.error(err)
             })
         }, []) // only run it once!
 
