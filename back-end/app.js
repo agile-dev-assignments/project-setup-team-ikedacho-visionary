ff285c8e15d247be41372187c184db5884150228
lh2510 <lh2510@nyu.edu>
2021-04-16 06:25:33 -0400

    Add: add edit post database #293

--- back-end/app.js
+++ back-end/app.js
@@ -46,9 +46,6 @@ app.use(passport.session());
  require("./loginAuth/passPortConfig.js")(passport);
 //----------------------------------------- END OF MIDDLEWARE---------------------------------------------------
 
-
-let post_data =[{"id":1,"source":"Twitter","content":"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.","senttime":"2/18/2021","contentimg":"http://dummyimage.com/238x249.png/cc0000/ffffff"},{"id":2,"source":"Facebook","content":"Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.","senttime":"2/17/2021","contentimg":"http://dummyimage.com/190x250.png/5fa2dd/ffffff"},{"id":3,"source":"Facebook","content":"Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.","senttime":"8/1/2020","contentimg":"http://dummyimage.com/187x154.png/ff4444/ffffff"},{"id":4,"source":"Instagram","content":"Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.","senttime":"12/1/2020","contentimg":"http://dummyimage.com/117x277.png/dddddd/000000"},{"id":5,"source":"Twitter","content":"Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.","senttime":"11/8/2020","contentimg":"http://dummyimage.com/200x111.png/dddddd/000000"},{"id":6,"source":"Facebook","content":"Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante.","senttime":"4/8/2020","contentimg":"http://dummyimage.com/204x133.png/dddddd/000000"},{"id":7,"source":"Twitter","content":"Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.","senttime":"2/13/2021","contentimg":"http://dummyimage.com/184x147.png/ff4444/ffffff"},{"id":8,"source":"Twitter","content":"Nullam porttitor lacus at turpis.","senttime":"8/6/2020","contentimg":"http://dummyimage.com/163x149.png/5fa2dd/ffffff"},{"id":9,"source":"Facebook","content":"Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.","senttime":"12/29/2020","contentimg":"http://dummyimage.com/182x256.png/ff4444/ffffff"},{"id":10,"source":"Instagram","content":"Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla.","senttime":"4/27/2020","contentimg":"http://dummyimage.com/182x193.png/dddddd/000000"},{"id":11,"source":"O-Zone","content":"itae non mattis pulvinar, ede ullamcorper auglit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur atellusemper inteMauris ulrpernulla.","senttime":"4/30/2020","contentimg":"http://dummyimage.com/182x193.png/dddddd/000000"}]//end of backup data
-
 let selected_social_media= ["O-Zone","Facebook", "Twitter","Instagram"]
 
 //put routes here:
@@ -74,7 +71,7 @@ app.post("/api_register", (req, res) => {
             following_number: 0,
             linked_social_media: ['O-Zone'],
             unconnected_social_media: ['Twitter','Instagram','Facebook'],
-            });
+        });
 
         // naive way to store current user info in session
         req.user = req.body.username
@@ -392,25 +389,34 @@ app.get("/follow", (req, res) => {
 
 })
 
-app.get("/get_edit", (req, res) => {
+app.get("/get_edit", async (req, res) => {
+    const my_username = req.user.username
     const post_text=req.query.post_text
     //console.log("post_text:",post_text)
-
-    //post_number plus 1
-    user_info.post_number = +user_info.post_number + 1 
-    //console.log( user_info.post_number )
-    //add the newly sent post to post_data in myprofile page
-    //{"id":1,"source":"Twitter","content":"lcuit amet turicula consequat.","senttime":"2/18/2021","contentimg":"http://dummyimage.com/238x249.png/cc0000/ffffff"}
-    const new_post={}
-    new_post.id= +post_data[0].id -1
-    new_post.source="O-Zone"
-    new_post.content= post_text
-    new_post.senttime = new Date().toLocaleString()
-    new_post.contentimg=" "
-    //console.log("before",post_data)
-    post_data.unshift(new_post)
-
-    //console.log("after",post_data)
+ 
+    await UserInfo.findOne({user_name: my_username}, async(err, UserInfos)=>{
+        try {
+            //const post_id=UserInfos.post_data[0].id
+            //new_post.id=post_id+1
+            UserInfos.post_data.unshift({
+                content:post_text,
+                source:"O-Zone",
+                senttime:new Date(),
+                contentimg:" ",
+            })
+            //UserInfos.post_number=+user_info.post_number + 1 
+            await UserInfos.save(function(saveErr, saveUserInfos) {
+                if(err){
+                    console.log('error saving post')
+                }
+                else{
+                    console.log(saveUserInfos);	
+                }
+            });   
+        } catch(e){
+            console.log(e)
+        }
+    })
 })
 
 
