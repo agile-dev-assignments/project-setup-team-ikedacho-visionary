b94a59f93010f6ce55067c8374b55264ee26feb0
xx708@nyu.edu <xx708@nyu.edu>
2021-04-17 01:26:38 +0800

    Update: minor syntax tweak

--- front-end/src/me/Browse_History.js
+++ front-end/src/me/Browse_History.js
@@ -4,34 +4,23 @@ import Browse from "../auxiliary/Browse";
 import "./Browse_History.css";
 import axios from "axios"
 const Browse_History = (props) => {
-    const [PostData, setPostData] = useState([
-        {
-            viewDate: "2021-4-1",
-            UserName: "asidjashdkasdhk9",
-            contentimgs: ["http://dummyimage.com/80x80.jpg/cc0000/ffffff",
-                "http://dummyimage.com/80x80.jpg/5fa2dd/ffffff"],
-            userimg: "https://robohash.org/etadipiscitempore.bmp?size=50x50\u0026set=set1",
-            content: "Post Content",
-        }
-    ])
+    const [PostData, setPostData] = useState([])
 
     useEffect(() => {
-    
-        axios('/api_browse')
+        axios
+            .get('/api_browse')
             .then((response) => {
                 // extract the data from the server response
                 setPostData(response.data)
             })
             .catch((err) => {
-               console.log(err);
+                console.error(err);
             })
-        }, []) // only run it once!
+    }, []) // only run it once!
 
-        console.log(PostData);
+    console.log(PostData);
         
-        
-
-          return (
+    return (
         <div className="Browse_History">
             <header>
                 <Link to="/Me">
@@ -42,20 +31,17 @@ const Browse_History = (props) => {
             </header>
 
             <div className="Browse_List">
-            {PostData.map((item) => (
-                console.log("rendering"),
-                <Browse 
-                    viewdate = {item.viewDate}
-                    UserName={item.UserName}
-                    userimg={item.userimg}
-                    content={item.content}
-                    contentimgs={item.contentimgs}
-                />
-                  
-            ))}
-               
+                {PostData.map((item) => (
+                    console.log("rendering"),
+                    <Browse 
+                        viewdate = {item.viewDate}
+                        UserName={item.UserName}
+                        userimg={item.userimg}
+                        content={item.content}
+                        contentimgs={item.contentimgs}
+                    /> 
+                ))}
             </div>
-
         </div>
     );
 
