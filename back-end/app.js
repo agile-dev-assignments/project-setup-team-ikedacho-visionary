bce83e83672454c04e4f4f2b1b76315182226317
xx708@nyu.edu <xx708@nyu.edu>
2021-04-17 01:27:53 +0800

    Update: UserInfo retrieval from database for /Me browse-history and liked history, /Community likes and mentions

--- back-end/app.js
+++ back-end/app.js
@@ -118,10 +118,10 @@ app.get("/api_get_user_info_by_name", (req, res) => {
 
 app.post("/browsed", (req, res, next) => {
     console.log(req.body); 
-    var myquery = { user_name: req.body.user_name };
-    var newvalues =  {$set: {my_history : {view_history: req.body.postId }}};
-    UserInfo.updateOne(myquery, newvalues, function(err, res) {
-        if (err) throw err;
+    var my_query = { user_name: req.body.user_name };
+    var new_values =  {$set: {my_history : {view_history: req.body.postId }}};
+    UserInfo.updateOne(my_query, new_values, function(err, res) {
+        if (err) throw err; 
         console.log("UserInfo successfully updated");
     });
     
@@ -132,12 +132,20 @@ app.post("/browsed", (req, res, next) => {
 
 
 app.get("/api_browse", (req, res) => {
+    let ret
+    const username = req.user.username
 
-    const browsedData = [{viewDate: "2021-4-1",UserName: "asidjashdkasdhk9",contentimgs: ["http://dummyimage.com/80x80.bmp/cc0000/ffffff","http://dummyimage.com/80x80.bmp/cc0000/ffffff"],userimg: "https://robohash.org/etadipiscitempore.bmp?size=50x50\u0026set=set1",content: "Post Content",},
-        {viewDate: "2021-4-1",UserName: "rmorcombe0",contentimgs: ["http://dummyimage.com/80x80.jpg/5fa2dd/ffffff","http://dummyimage.com/80x80.jpg/5fa2dd/ffffff","http://dummyimage.com/80x80.jpg/5fa2dd/ffffff"],userimg: "https://robohash.org/etillumet.bmp?size=50x50\u0026set=set1",content: "Post Content",
-        }];
-    console.log(browsedData)
-    res.json(browsedData); // The req.user stores the entire user that has been authenticated inside of it.
+    // retrieve data from database
+    UserInfo.findOne({user_name: username}, (err, result) => {
+        if (err) {
+            console.error(err)
+        } else {
+            // extract the brose history field
+            ret = result.my_browse_history
+            console.log(ret)
+            res.json(ret)
+        }
+    })
 });
 
 
@@ -293,14 +301,14 @@ app.get("/get_my_profile", async (req, res) => {
         }
      })
 
-    //filtered_post_data by selected platfrom_name by user 
+    //filtered_post_data by selected platform_name by user 
     if (req.query.platform_name_array!==undefined) {
         //console.log("111111")
         filtered_post_data=post_data.filter(element=>{
            if (req.query.platform_name_array.includes(element.source)){
                return true
            }
-        })//end of filtered_post_data by selected platfrom_name by user 
+        })//end of filtered_post_data by selected platform_name by user 
     }
 
     //send back response_data which consists of user_info and filtered_post_data as post_data
@@ -320,7 +328,7 @@ app.get("/get_my_profile", async (req, res) => {
 
 
 // it tell multer to save uploaded files to disk into a directory named public/uploads, with a filename based on the current time.
-// the file storage rule function referred by a varibale called storage will be used later as parameter when we initiated a multer object.
+// the file storage rule function referred by a variable called storage will be used later as parameter when we initiated a multer object.
 const storage = multer.diskStorage({
     // set file saved destination. Multer can save uploaded files to a number of different destinations.
     destination: function (req, file, cb) {
@@ -614,29 +622,33 @@ app.get("/api_friend_suggestion", async (req, res) => {
 app.get("/api_being_liked", async (req, res) => {
     let ret = {}
 
-    await axios
-        .get(`${process.env.API_BEING_LIKED}?key=${process.env.API_BEING_LIKED_KEY}`)
-        .then(apiResponse => ret = apiResponse.data)
-        .catch((err) => {
-            const backupData = [{"id":1,"post_text":"Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit.","post_image":"http://dummyimage.com/80x80.jpg/5fa2dd/ffffff","post_date":"10/31/2020","liked_by_username":"Express.js like_history","liked_by_profile_image":"https://robohash.org/sedillumquis.jpg?size=50x50\u0026set=set1","liked_date":"5/2/2020"},{"id":2,"post_text":"Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum.","post_image":"http://dummyimage.com/80x80.png/5fa2dd/ffffff","post_date":"12/21/2020","liked_by_username":"fbaterip1","liked_by_profile_image":"https://robohash.org/estdoloreest.jpg?size=50x50\u0026set=set1","liked_date":"3/16/2020"},{"id":3,"post_text":"Morbi non quam nec dui luctus rutrum.","post_image":"http://dummyimage.com/80x80.jpg/ff4444/ffffff","post_date":"11/13/2020","liked_by_username":"fcopello2","liked_by_profile_image":"https://robohash.org/molestiaemaximenumquam.png?size=50x50\u0026set=set1","liked_date":"8/13/2020"},{"id":4,"post_text":"Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.","post_image":"http://dummyimage.com/80x80.png/5fa2dd/ffffff","post_date":"6/8/2020","liked_by_username":"pgreenalf3","liked_by_profile_image":"https://robohash.org/corporisillumrerum.jpg?size=50x50\u0026set=set1","liked_date":"9/14/2020"},{"id":5,"post_text":"Phasellus in felis.","post_image":"http://dummyimage.com/80x80.png/dddddd/000000","post_date":"12/5/2020","liked_by_username":"iseebright4","liked_by_profile_image":"https://robohash.org/inciduntuttenetur.png?size=50x50\u0026set=set1","liked_date":"11/21/2020"},{"id":6,"post_text":"Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc.","post_image":"http://dummyimage.com/80x80.jpg/ff4444/ffffff","post_date":"5/2/2020","liked_by_username":"gbourley5","liked_by_profile_image":"https://robohash.org/evenietconsequunturlabore.png?size=50x50\u0026set=set1","liked_date":"9/26/2020"},{"id":7,"post_text":"Nulla ut erat id mauris vulputate elementum. Nullam varius.","post_image":"http://dummyimage.com/80x80.jpg/5fa2dd/ffffff","post_date":"4/25/2020","liked_by_username":"mutteridge6","liked_by_profile_image":"https://robohash.org/quoindignissimos.jpg?size=50x50\u0026set=set1","liked_date":"4/10/2020"},{"id":8,"post_text":"Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.","post_image":"http://dummyimage.com/80x80.jpg/dddddd/000000","post_date":"11/23/2020","liked_by_username":"mpreece7","liked_by_profile_image":"https://robohash.org/accusantiumminimaquo.bmp?size=50x50\u0026set=set1","liked_date":"9/3/2020"},{"id":9,"post_text":"Mauris sit amet eros.","post_image":"http://dummyimage.com/80x80.png/dddddd/000000","post_date":"5/14/2020","liked_by_username":"elowres8","liked_by_profile_image":"https://robohash.org/cumquenecessitatibustempore.bmp?size=50x50\u0026set=set1","liked_date":"4/29/2020"},{"id":10,"post_text":"Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.","post_image":"http://dummyimage.com/80x80.bmp/cc0000/ffffff","post_date":"11/11/2020","liked_by_username":"aclarridge9","liked_by_profile_image":"https://robohash.org/quiasequirepellat.png?size=50x50\u0026set=set1","liked_date":"1/22/2021"}]
-            ret = backupData
-        })
-
-    res.json(ret)
+    // retrieve user info from database
+    UserInfo.findOne({user_name: req.user.username}, (err, result) => {
+        if (err) {
+            console.error(err)
+        } else {
+            // extract being liked history
+            ret = result.others_liked_history
+            console.log(ret)
+            res.json(ret)
+        }
+    })
 })
 
 app.get("/api_being_mentioned", async (req, res) => {
     let ret = {}
 
-    await axios
-        .get(`${process.env.API_BEING_MENTIONED}?key=${process.env.API_BEING_MENTIONED_KEY}`)
-        .then(apiResponse => ret = apiResponse.data)
-        .catch((err) => {
-            const backupData = [{"mentioner_avatar":"https://robohash.org/veniamrepellatsimilique.png?size=50x50\u0026set=set1","mentioner_username":"rleadley0","mentioned_date":"09/11/2020","post_image":"http://dummyimage.com/100x100.png/cc0000/ffffff","post_username":"oheigl0","post_text":"In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst."},{"mentioner_avatar":"https://robohash.org/excepturinullaqui.png?size=50x50\u0026set=set1","mentioner_username":"fpikhno1","mentioned_date":"11/01/2020","post_image":"http://dummyimage.com/100x100.png/ff4444/ffffff","post_username":"hbarrable1","post_text":"Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros."},{"mentioner_avatar":"https://robohash.org/praesentiumquasut.png?size=50x50\u0026set=set1","mentioner_username":"amccolley2","mentioned_date":"01/26/2021","post_image":"http://dummyimage.com/100x100.png/dddddd/000000","post_username":"sdahlbom2","post_text":"Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio."},{"mentioner_avatar":"https://robohash.org/odioquiavero.png?size=50x50\u0026set=set1","mentioner_username":"bschnitter3","mentioned_date":"11/29/2020","post_image":"http://dummyimage.com/100x100.png/5fa2dd/ffffff","post_username":"rrymour3","post_text":"In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet."},{"mentioner_avatar":"https://robohash.org/magniquasisapiente.png?size=50x50\u0026set=set1","mentioner_username":"ccopello4","mentioned_date":"09/14/2020","post_image":"http://dummyimage.com/100x100.png/dddddd/000000","post_username":"pmains4","post_text":"Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl."},{"mentioner_avatar":"https://robohash.org/deseruntperspiciatisoccaecati.png?size=50x50\u0026set=set1","mentioner_username":"bvergo5","mentioned_date":"05/29/2020","post_image":"http://dummyimage.com/100x100.png/ff4444/ffffff","post_username":"flaraway5","post_text":"Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam."},{"mentioner_avatar":"https://robohash.org/maioreseumquibusdam.png?size=50x50\u0026set=set1","mentioner_username":"ewillerson6","mentioned_date":"06/28/2020","post_image":"http://dummyimage.com/100x100.png/cc0000/ffffff","post_username":"mwhylie6","post_text":"Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."},{"mentioner_avatar":"https://robohash.org/nonadipiscivel.png?size=50x50\u0026set=set1","mentioner_username":"wplayfair7","mentioned_date":"02/25/2021","post_image":"http://dummyimage.com/100x100.png/5fa2dd/ffffff","post_username":"rarnaldo7","post_text":"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."},{"mentioner_avatar":"https://robohash.org/utconsequaturnam.png?size=50x50\u0026set=set1","mentioner_username":"hmckibbin8","mentioned_date":"01/28/2021","post_image":"http://dummyimage.com/100x100.png/ff4444/ffffff","post_username":"cloudian8","post_text":"Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque."},{"mentioner_avatar":"https://robohash.org/similiquevoluptasassumenda.png?size=50x50\u0026set=set1","mentioner_username":"lmandrake9","mentioned_date":"06/27/2020","post_image":"http://dummyimage.com/100x100.png/ff4444/ffffff","post_username":"lhakeworth9","post_text":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis."}]
-            ret = backupData
-        })
-
-    res.json(ret)
+    // retrieve data from database
+    UserInfo.findOne({user_name: req.user.username}, (err, result) => {
+        if (err) {
+            console.error(err)
+        } else {
+            // extract mentioned history
+            ret = result.others_mentioned_history
+            console.log(ret)
+            res.json(ret)
+        }
+    })
 })
 
 app.get("/api_liked_history", async (req, res) => {
@@ -691,22 +703,6 @@ app.get("/api_chat_messages", async (req, res) => {
             res.json(ret)
         }
     })
-
-    /* 
-    For this API, as for March 30, we still not have database yet, so I use hard coded backup data directly
-    with API_CHAT_MESSAGES and API_CHAT_MESSAGES_KEY actually being empty string
-    */
-    /*
-    await axios
-        .get(`${process.env.API_CHAT_MESSAGES}?key=${process.env.API_CHAT_MESSAGES_KEY}`)
-        .then(apiResponse => ret = apiResponse.data)
-        .catch((err) => {
-            const user_photo = "https://robohash.org/laboriosamaliquamconsequuntur.jpg?size=50x50\u0026set=set1"
-            const user_name = "ChatHistoryFakeName"
-            const backupData = [{"userimg": `${user_photo}`,"username": `${user_name}`,"time": "03/22/2021 11:00 AM","fromSender": true,"content": "Hi. "},{"userimg": "https://gravatar.com/avatar/412dd18bd4b3e7b5ff96752e42a767c9?s=200&d=robohash&r=x","username": "Tom","time": "03/22/2021 11:01 AM","fromSender": false,"content": "Hi there. "},{"userimg": `${user_photo}`,"username": `${user_name}`,"time": "03/22/2021 11:02 AM","fromSender": `${true}`,"content": "How are you? "},{"userimg": "https://gravatar.com/avatar/412dd18bd4b3e7b5ff96752e42a767c9?s=200&d=robohash&r=x","username": "Tom","time": "03/22/2021 2:00 PM","fromSender": false,"content": "Bye... "},{"userimg": `${user_photo}`,"username": `${user_name}`,"time": "03/22/2021 2:05 PM","fromSender": true,"content": "Ok... "}]
-            ret = backupData
-        })
-    */
 })
 
 app.post("/api_send_new_message", async (req, res) => {
@@ -716,6 +712,7 @@ app.post("/api_send_new_message", async (req, res) => {
     const self_userimg = req.body.userimg
     console.log(newMessage, roomID)
 
+    // generate a date string in pretty format
     const message_date = currentdate.getFullYear() + "/"
                          + (currentdate.getMonth() + 1)  + "/" 
                          + currentdate.getDate() + " "  
@@ -725,6 +722,7 @@ app.post("/api_send_new_message", async (req, res) => {
 
     console.log(message_date)
 
+    // find the chat room, and then add the new message to message history
     Chatroom.findOne({_id: roomID}, (err, result) => {
         result.message_history.push({
             userimg: self_userimg, 
@@ -733,6 +731,7 @@ app.post("/api_send_new_message", async (req, res) => {
             content: newMessage
         })
         console.log(result)
+        // save the update
         result.save((err) => {
             if (err) {
                 console.log(err)
