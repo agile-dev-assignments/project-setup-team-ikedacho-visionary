531fff09370fb9dc21507b213d3aaa4f138d9de5
xx708@nyu.edu <xx708@nyu.edu>
2021-04-19 21:09:50 +0800

    Fix: user without friend will have empty create-new-chat selection list, instead of an error

--- back-end/app.js
+++ back-end/app.js
@@ -1269,38 +1269,46 @@ app.post('/api_send_new_message', async (req, res) => {
 app.get('/api_create_new_chat_list', async (req, res) => {
     let ret = [],
         friend = [],
-        follower,
-        following
+        follower = [],
+        following = []
 
     // retrieve follower and following lists of current user
     await UserInfo.findOne({ user_name: req.user.username }, (err, result) => {
         if (err) {
             console.error(err)
         } else {
-            follower = result.follower
-            following = result.following
+            follower.push(result.follower)
+            following.push(result.following)
         }
     })
 
-    // lazy finding intersection
-    for (let i = 0; i < follower.length; i++) {
-        if (following.includes(follower[i])) {
-            friend.push(follower[i])
-        }
-    }
-
-    // retrieve extended user info from database
-    for (let i = 0; i < friend.length; i++) {
-        await UserInfo.findOne({ user_name: friend[i] }, (err, result) => {
-            if (err) {
-                console.error(err)
-            } else {
-                ret.push({
-                    user_name: result.user_name,
-                    user_photo: result.user_photo,
-                })
+    console.log("follower: ", follower)
+    console.log("following: ", following)
+    console.log(!isEmpty(follower[0]) && !isEmpty(following[0]))
+    if (!isEmpty(follower[0]) && !isEmpty(following[0])){    
+        // lazy finding intersection
+        console.log("friend: ", friend)
+        for (let i = 0; i < follower[0].length; i++) {
+            if (following[0].includes(follower[0][i])) {
+                friend.push(follower[0][i])
             }
-        })
+        }
+
+        console.log("friend after intersection: ", friend)
+
+        // retrieve extended user info from database
+        for (let i = 0; i < friend.length; i++) {
+            await UserInfo.findOne({ user_name: friend[i] }, (err, result) => {
+                if (err) {
+                    console.error(err)
+                } else {
+                    ret.push({
+                        user_name: result.user_name,
+                        user_photo: result.user_photo,
+                    })
+                }
+            })
+        }
     }
 
     console.log(ret)
