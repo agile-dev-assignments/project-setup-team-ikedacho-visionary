8c6035a7175d6c657c8e8380ec7fb902c301cd35
lh2510 <57370805+lh2510@users.noreply.github.com>
2021-04-19 12:46:22 -0400

    Revert "placeholder commit: https config and syntax change"

--- back-end/server.js
+++ back-end/server.js
@@ -5,24 +5,6 @@ const port = 3000 // the port to listen to for incoming requests
 const listener = server.listen(port, function () {
     console.log(`Server running on port: ${port}`)
 })
-;('use strict')
-
-var app = require('./app.js')
-
-require('greenlock-express')
-    .init({
-        packageRoot: __dirname,
-        configDir: './greenlock.d',
-
-        // contact for security and critical bug notices
-        maintainerEmail: 'lh2510@nyu.edu',
-
-        // whether or not to run at cloudscale
-        cluster: false,
-    })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app)
 // a function to stop listening to the port
 const close = () => {
     listener.close()
