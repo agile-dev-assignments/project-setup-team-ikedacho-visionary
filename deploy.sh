38eb3b74da63b07ba9b273c990ae8e4baff0a4e1
lh2510 <lh2510@nyu.edu>
2021-04-28 17:07:11 -0400

    minor change

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 set -xe
 
-if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
+  echo "1."
   ssh-add ~/.ssh/id_rsa
 
   cd ~
   git clone https://github.com/agile-dev-assignments/project-setup-team-ikedacho-visionary.git
   cd project-setup-team-ikedacho-visionary
   pm2 start npm -- start
-else
-  echo "Not deploying, since the branch isn't master."
-fi
+
+  echo "3."
+ssh Lin@167.172.155.162 'pm2 restart all'
