95a7b6eb4d72751b7bae2a28aef4d17550303534
xx708@nyu.edu <xx708@nyu.edu>
2021-04-28 11:46:07 +0800

    Update: to_twitter fix as Twitter API is finally back onlinegit add back-end/router/me/twitter.js !

--- back-end/router/me/twitter.js
+++ back-end/router/me/twitter.js
@@ -20,14 +20,14 @@ twitterRouter.get('/', async (req, res) => {
         oauth_timestamp: date,
         oauth_signature_method: 'HMAC-SHA1',
         oauth_version: '1.0',
-        oauth_callback: 'http://localhost:4000/me',
+        oauth_callback: 'http://localhost:4000/to_twitter',
     }
     const request_url = 'https://api.twitter.com/oauth/request_token'
     const consumerSecret = process.env.TWITTER_API_SECRET_KEY
     const tokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET
     const signature = oauthSignature.generate('POST', request_url, parameters, consumerSecret, tokenSecret)
 
-    const AuthHeader = `OAuth oauth_consumer_key="${process.env.TWITTER_API_KEY}",oauth_token="${process.env.TWITTER_ACCESS_TOKEN}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${date}",oauth_nonce="${nonce}",oauth_version="1.0",oauth_callback="http%3A%2F%2Flocalhost%3A4000%2Fme",oauth_signature="${signature}"`
+    const AuthHeader = `OAuth oauth_consumer_key="${process.env.TWITTER_API_KEY}",oauth_token="${process.env.TWITTER_ACCESS_TOKEN}",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${date}",oauth_nonce="${nonce}",oauth_version="1.0",oauth_callback="http%3A%2F%2Flocalhost%3A4000%2Fto_twitter",oauth_signature="${signature}"`
 
     // HTTPS request
     const options = {
@@ -42,6 +42,7 @@ twitterRouter.get('/', async (req, res) => {
         if (error) {
             console.error(err)
         } else {
+            console.log(result.body)
             // manually parse the returned string
             let arr = result.body.split('&')
             for (let i = 0; i < 3; i++) {
@@ -53,4 +54,15 @@ twitterRouter.get('/', async (req, res) => {
         }
     })
 })
+
+function fast_hash(str) {
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(i)
+        hash = (hash << 5) - hash + char
+        hash &= hash
+    }
+    return hash
+}
+
 module.exports = twitterRouter
