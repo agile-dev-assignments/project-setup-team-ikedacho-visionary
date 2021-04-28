c7cd9abde9385c0ffd3654c237f133a0e610daa0
lh2510 <lh2510@nyu.edu>
2021-04-28 16:59:31 -0400

    minor change

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@<droplet ipaddress>:/home/<sudo user>/demo/client
-  rsync -a server/ travis@<droplet ipaddress>:/home/<sudo user>/demo/server
+  cd ~
+  git clone https://github.com/agile-dev-assignments/project-setup-team-ikedacho-visionary.git
+  cd project-setup-team-ikedacho-visionary
+  pm2 start npm -- start
 else
   echo "Not deploying, since the branch isn't master."
 fi
