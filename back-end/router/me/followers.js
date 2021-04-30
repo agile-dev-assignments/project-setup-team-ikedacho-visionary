2c555e2ae330ef354ffda987569e87d305405cbc
xx708@nyu.edu <xx708@nyu.edu>
2021-04-30 13:06:30 +0800

    Update: fix on empty array returning

--- back-end/router/me/followers.js
+++ back-end/router/me/followers.js
@@ -7,34 +7,28 @@ followersRouter.get('/', async (req, res) => {
         ret = []
     const UserName = req.query.UserName
 
+    console.log(UserName)
+
     // find user object in database and extract its follower array
-    await UserInfo.findOne({ user_name: UserName }, (err, result) => {
-        if (err) {
-            console.error(err)
-        } else {
-            follower_list = result.follower
-            following_list = result.following
-        }
+    const result = await UserInfo.findOne({ user_name: UserName })
+    follower_list = result.follower
+    following_list = result.following
+
+    console.log(follower_list.length)
+
+    // find user infos of the follower list
+    const users = await UserInfo.find({ user_name: { $in: follower_list } })
+    users.forEach((user) => {
+        ret.push({
+            user_name: user.user_name,
+            bio: user.bio,
+            user_photo: user.user_photo,
+            // dynamically decide the action based on the following status
+            action: following_list.includes(user.user_name) ? 'Unfollow' : 'Follow',
+        })  
     })
 
-    // retrieve extended user info from database
-    for (let i = 0; i < follower_list.length; i++) {
-        await UserInfo.findOne({ user_name: follower_list[i] }, (err, result) => {
-            if (err) {
-                console.error(err)
-            } else {
-                ret.push({
-                    user_name: result.user_name,
-                    bio: result.bio,
-                    user_photo: result.user_photo,
-                    // dynamically decide the action based on the following status
-                    action: following_list.includes(result.user_name) ? 'Unfollow' : 'Follow',
-                })
-            }
-        })
-    }
-
-    console.log('Follower info\n: ', ret)
+    console.log('Follower info\n: ', ret.length)
     res.json(ret)
 })
 module.exports = followersRouter
