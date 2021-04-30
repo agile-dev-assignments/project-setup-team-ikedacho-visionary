af883f4d151e3a14c0ea7fa59f4df9a81ab68c3b
lh2510 <lh2510@nyu.edu>
2021-04-30 01:38:00 -0400

    Update: DC

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,29 @@
 set -xe
+  
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+ssh-add -l
 
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_rsa
+#ssh travis@134.209.208.110  
+#cd ~/demo
 
-  cd ~
+#if [ -d "project-setup-team-ikedacho-visionary" ] ; then
+#    echo "Directory project-setup-team-ikedacho-visionary exists."
+#    cd project-setup-team-ikedacho-visionary
+#    git pull
+#  else
+#    echo "Cloning project-setup-team-ikedacho-visionary....."
+#    git clone https://github.com/agile-dev-assignments/project-setup-team-ikedacho-visionary.git
+#    cd project-setup-team-ikedacho-visionary
+#  fi
 
-  if [ -d "project-setup-team-ikedacho-visionary" ] ; then
-    echo "Directory project-setup-team-ikedacho-visionary exists."
-    cd project-setup-team-ikedacho-visionary
-    git pull https://github.com/agile-dev-assignments/project-setup-team-ikedacho-visionary.git
-  else
-    echo "Cloning project-setup-team-ikedacho-visionary....."
-    git clone https://github.com/agile-dev-assignments/project-setup-team-ikedacho-visionary.git
-    cd project-setup-team-ikedacho-visionary
-  fi
-  echo "the branch isn't master."
+#  cd front-end
+#  npm install
+#  npm run build
 
-  cd front-end
-  npm install
-
-  cd ../back-end
-  npm install
-  echo "in back-end. start pm2..."
-  pm2 start npm -- start
-  echo "3."
+#  cd ../back-end
+#  npm install
+#  echo "in back-end. start pm2..."
+#  pm2 start server.js --name "server"
+ 
+echo "finish"
