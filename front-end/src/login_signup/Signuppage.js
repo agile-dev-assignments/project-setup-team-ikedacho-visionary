adcaca520218286e231e93da8f637eb2c3c7ad37
clairemaa <xm572@nyu.edu>
2021-05-03 15:24:00 +0800

    backbutton

--- front-end/src/login_signup/Signuppage.js
+++ front-end/src/login_signup/Signuppage.js
@@ -1,10 +1,23 @@
 import Signup from '../auxiliary/Signupform'
 import './Loginpage.css'
 import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 function Signuppage() {
+
+    let history = useHistory()
+
+    const goTOPreviousPath = () => {
+        history.goBack()
+    }
     return (
         <div className='Loginpage'>
+            <section id='header'>
+            <Link onClick={goTOPreviousPath}>
+                <h1 id='back'>Back</h1>
+            </Link>
+            </section>
             <div class='header__text'>
                 <h1>O-Zone</h1>
                 <p>Sign in or create a new account.</p>
