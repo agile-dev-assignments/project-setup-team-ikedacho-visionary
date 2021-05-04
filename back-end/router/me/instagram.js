e71e69b85ad3dba7654c08820e9b2b9a82275711
lh2510 <lh2510@nyu.edu>
2021-05-04 04:01:55 -0400

    fix

--- back-end/router/me/instagram.js
+++ back-end/router/me/instagram.js
@@ -42,71 +42,69 @@ instagramRouter.get('/', async (req, res) => {
         let client_secret = '85832b0f1c6eac1c7471a657f365b1e3'
         let redirect_uri = 'https://ozonewebapp.com/instagram_auth/'
         const my_username = req.user.username
-        if (req.query.code) {
-            const code = req.query.code
-            console.log('code:', code)
-            res.status(200).send()
 
-            //Exchange the Code for a Token
-            var options = {
-                url: 'https://api.instagram.com/oauth/access_token',
-                method: 'POST',
-                form: {
-                    client_id: client_id,
-                    client_secret: client_secret,
-                    grant_type: 'authorization_code',
-                    redirect_uri: redirect_uri,
-                    code: req.query.code,
-                },
-            }
+        let url = req.query
+        console.log(url)
 
-            request(options, function async(error, response, body) {
-                if (error) {
-                    console.log('error')
-                } else {
-                    const res = JSON.parse(body)
-                    console.log('get short-lived-token(body)', JSON.parse(body))
-                    //get short lived access token
-                    short_lived_accessToken = res.access_token
-                    //get userid
-                    user_id = res.user_id
+        const code = req.query.code
+        console.log('code:', code)
 
-                    //get short lived access token
-                    request(
-                        `https://graph.instagram.com/access_token=${short_lived_accessToken}`,
-
-                        async function (error, response, body) {
-                            if (error) {
-                                console.log('error')
-                            } else {
-                                const res = JSON.parse(body)
-                                console.log('get long-lived-token(body)', JSON.parse(body))
-                                long_lived_token = res.access_token
-
-                                //get userid
-                                request(
-                                    `https://graph.instagram.com/me?fields=id,username&access_token=${short_lived_accessToken}`,
-
-                                    async function (error, response, body) {
-                                        if (error) {
-                                            console.log('error')
-                                        } else {
-                                            const res = JSON.parse(body)
-                                            console.log('get username', JSON.parse(body))
-                                            const username = res.username
-                                            console.log(username)
-                                        }
-                                    }
-                                )
-                            }
-                        }
-                    )
-                }
-            })
-        } else {
-            console.log('fail')
-            res.status(500).send()
+        //Exchange the Code for a Token
+        var options = {
+            url: 'https://api.instagram.com/oauth/access_token',
+            method: 'POST',
+            form: {
+                client_id: client_id,
+                client_secret: client_secret,
+                grant_type: 'authorization_code',
+                redirect_uri: redirect_uri,
+                code: req.query.code,
+            },
         }
+
+        request(options, function async(error, response, body) {
+            if (error) {
+                console.log('error')
+            } else {
+                const res = JSON.parse(body)
+                console.log('get short-lived-token(body)', JSON.parse(body))
+                //get short lived access token
+                short_lived_accessToken = res.access_token
+                //get userid
+                user_id = res.user_id
+
+                //get short lived access token
+                request(
+                    `https://graph.instagram.com/access_token=${short_lived_accessToken}`,
+
+                    async function (error, response, body) {
+                        if (error) {
+                            console.log('error')
+                        } else {
+                            const res = JSON.parse(body)
+                            console.log('get long-lived-token(body)', JSON.parse(body))
+                            long_lived_token = res.access_token
+
+                            //get userid
+                            request(
+                                `https://graph.instagram.com/me?fields=id,username&access_token=${short_lived_accessToken}`,
+
+                                async function (error, response, body) {
+                                    if (error) {
+                                        console.log('error')
+                                    } else {
+                                        const res = JSON.parse(body)
+                                        console.log('get username', JSON.parse(body))
+                                        const username = res.username
+                                        console.log(username)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                )
+            }
+        })
     }
 })
 module.exports = instagramRouter
